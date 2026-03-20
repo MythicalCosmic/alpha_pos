077fd41c1a412b1c2686ce02705f9070c6e900be
Cosmic <qodirjonov0854@gmail.com>
2026-03-20 13:06:52 +0500

    before order serivice added

---
+++ base/services/inkassa_service.py
@@ -0,0 +1,11 @@
+from base.repositories import CashRegisterRepository
+
+
+class InkassaService:
+    @staticmethod
+    def add_to_register(amount):
+        register = CashRegisterRepository.get_current()
+        if not register:
+            return
+        register.current_balance += amount
+        register.save(update_fields=['current_balance', 'last_updated'])
